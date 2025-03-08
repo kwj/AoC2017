9db9d6b3ede0137af558fd0296d68d136fb10be8
kwj <kwj@vlax.net>
2025-03-08 14:20:00 -0800

    Add util module

---
+++ src/util.cpp
@@ -0,0 +1,56 @@
+module;
+
+#include <string>
+#include <regex>
+#include <vector>
+
+// ----
+export module util;
+
+export namespace util {
+
+std::string getLine(std::stringstream &ss);
+std::vector<int> getNumbers(std::string const &s);
+std::vector<int> getUNumbers(std::string const &s);
+
+} // namespace util
+
+// ----
+module :private;
+
+namespace util {
+
+std::string getLine(std::stringstream &ss) {
+    std::string line;
+    std::getline(ss, line);
+
+    return line;
+}
+
+std::vector<int> getNumbers(std::string const &s) {
+    std::string src{s};
+    std::regex re(R"(\d+)");
+    std::vector<int> result;
+
+    for (std::smatch m; std::regex_search(src, m, re);) {
+        result.push_back(std::stoi(m.str()));
+        src = m.suffix();
+    }
+
+    return result;
+}
+
+std::vector<int> getUNumbers(std::string const &s) {
+    std::string src{s};
+    std::regex re(R"(-?\d+)");
+    std::vector<int> result;
+
+    for (std::smatch m; std::regex_search(src, m, re);) {
+        result.push_back(std::stoi(m.str()));
+        src = m.suffix();
+    }
+
+    return result;
+}
+
+} // namespace util
