d9b3674912cf9cbd0375f80854a0eec03570911c
kwj <kwj@vlax.net>
2025-03-25 11:10:35 -0700

    Add clang-format support to CMake

---
+++ cmake/clang-format.cmake
@@ -0,0 +1,17 @@
+find_program(CLANG_FORMAT_EXE clang-format)
+
+if(CLANG_FORMAT_EXE)
+    file(GLOB_RECURSE SOURCES
+        "src/*.cpp"
+    )
+
+    add_custom_target(format
+        COMMAND ${CLANG_FORMAT_EXE} -i ${SOURCES}
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    )
+
+    add_custom_target(format-check
+        COMMAND ${CLANG_FORMAT_EXE} --dry-run ${SOURCES}
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+    )
+endif()
