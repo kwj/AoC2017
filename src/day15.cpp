14ce1f4de01788839799799197216920b92187d5
kwj <kwj@vlax.net>
2025-03-26 03:42:15 -0700

    Use proper integer types

--- src/day15.cpp
+++ src/day15.cpp
@@ -13,6 +13,7 @@ module;
 #include <ranges>
 #include <string>
 #include <tuple>
+#include <utility>
 #include <vector>
 
 import util;
@@ -22,10 +23,10 @@ export module day15;
 
 export namespace day15 {
 
-std::tuple<unsigned long, unsigned long> solve(std::istream &is);
-std::tuple<unsigned long, unsigned long> parse(std::istream &is);
-unsigned long part1(std::tuple<unsigned long, unsigned long> const &result);
-unsigned long part2(std::tuple<unsigned long, unsigned long> const &result);
+std::tuple<long, long> solve(std::istream &is);
+std::pair<long, long> parse(std::istream &is);
+long part1(std::pair<long, long> const &result);
+long part2(std::pair<long, long> const &result);
 
 } // namespace day15
 
@@ -34,14 +35,14 @@ module :private;
 
 namespace day15 {
 
-std::tuple<unsigned long, unsigned long>
+std::tuple<long, long>
 solve(std::istream &is) {
     auto input_data = parse(is);
 
     return {part1(input_data), part2(input_data)};
 }
 
-std::tuple<unsigned long, unsigned long>
+std::pair<long, long>
 judgement(unsigned long num_a, unsigned long num_b) {
     constexpr long P1_N_PAIRS {40'000'000};
     constexpr long P2_N_PAIRS {5'000'000};
@@ -49,8 +50,8 @@ judgement(unsigned long num_a, unsigned long num_b) {
     constexpr unsigned long B {48271};
     constexpr unsigned long M {0x7FFFFFFF};
 
-    std::vector<unsigned long> mul4;
-    std::vector<unsigned long> mul8;
+    std::vector<unsigned short> mul4;
+    std::vector<unsigned short> mul8;
     mul4.reserve(P2_N_PAIRS);
     mul8.reserve(P2_N_PAIRS);
 
@@ -63,8 +64,8 @@ judgement(unsigned long num_a, unsigned long num_b) {
         num_a = (num_a * A) % M;
         num_b = (num_b * B) % M;
 
-        auto a = num_a & 0xFFFF;
-        auto b = num_b & 0xFFFF;
+        auto a = static_cast<unsigned short>(num_a & 0xFFFF);
+        auto b = static_cast<unsigned short>(num_b & 0xFFFF);
         if (p1_cnt < P1_N_PAIRS && a == b) {
             ++judge_p1;
         }
@@ -81,8 +82,8 @@ judgement(unsigned long num_a, unsigned long num_b) {
     }
 
     long judge_p2 {0};
-    for (auto tpl : std::views::zip(mul4, mul8)) {
-        if (std::get<0>(tpl) == std::get<1>(tpl)) {
+    for (auto const [x, y] : std::views::zip(mul4, mul8)) {
+        if (x == y) {
             ++judge_p2;
         }
     }
@@ -90,7 +91,7 @@ judgement(unsigned long num_a, unsigned long num_b) {
     return {judge_p1, judge_p2};
 }
 
-std::tuple<unsigned long, unsigned long>
+std::pair<long, long>
 parse(std::istream &is) {
     std::vector<unsigned long> seeds;
     for (std::string line; std::getline(is, line);) {
@@ -101,14 +102,14 @@ parse(std::istream &is) {
     return judgement(seeds[0], seeds[1]);
 }
 
-unsigned long
-part1(std::tuple<unsigned long, unsigned long> const &result) {
-    return std::get<0>(result);
+long
+part1(std::pair<long, long> const &result) {
+    return result.first;
 }
 
-unsigned long
-part2(std::tuple<unsigned long, unsigned long> const &result) {
-    return std::get<1>(result);
+long
+part2(std::pair<long, long> const &result) {
+    return result.second;
 }
 
 } // namespace day15
