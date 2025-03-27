4d88da508b210872e876c1a5edcd5cc72a511134
kwj <kwj@vlax.net>
2025-03-27 16:55:53 -0700

    Improve performance

--- src/day10.cpp
+++ src/day10.cpp
@@ -4,7 +4,9 @@ module;
 #include <format>
 #include <functional>
 #include <istream>
+#include <iterator>
 #include <numeric>
+#include <ranges>
 #include <sstream>
 #include <string>
 #include <string_view>
@@ -30,7 +32,7 @@ module :private;
 
 namespace day10 {
 
-constexpr long KNOTS_LEN {256};
+constexpr unsigned long KNOTS_LEN {256};
 
 std::tuple<long, std::string>
 solve(std::istream &is) {
@@ -45,7 +47,7 @@ parse(std::istream &is) {
 }
 
 std::vector<long>
-knotHash(std::vector<long> lengths, long cnt) {
+knotHash(std::vector<unsigned long> lengths, long round) {
     auto knots = std::vector<long>(KNOTS_LEN);
 
 // P2440R1 (ranges::iota) is not yet supported in libc++ 19.
@@ -55,51 +57,54 @@ knotHash(std::vector<long> lengths, long cnt) {
     std::iota(knots.begin(), knots.end(), 0);
 #endif
 
-    long skip {0};
-    long origin {0};
-    for (auto round = 0; round < cnt; ++round) {
+    unsigned long skip {0};
+    unsigned long pos {0};
+    while (round-- > 0) {
         for (auto const len : lengths) {
-            std::reverse(knots.begin(), knots.begin() + len);
+            // reverse
+            auto i1 = pos;
+            auto i2 = (pos + len - 1) % KNOTS_LEN;
+            for (unsigned long cnt = 0; cnt < len / 2; ++cnt) {
+                std::swap(knots[i1], knots[i2]);
+                i1 = (i1 + 1) % KNOTS_LEN;
+                i2 = i2 > 0 ? i2 - 1 : KNOTS_LEN - 1;
+            }
 
-            auto offset = (len + skip) % KNOTS_LEN;
-            std::rotate(knots.begin(), knots.begin() + offset, knots.end());
-            origin += offset;
+            // move position
+            pos = (pos + len + skip) % KNOTS_LEN;
             ++skip;
         }
     }
-
-    std::rotate(knots.begin(), knots.end() - (origin % KNOTS_LEN), knots.end());
 
     return knots;
 }
 
 long
 part1(std::string_view s) {
-    auto hashed_data = knotHash(util::getNumbers(s), 1);
+    auto hashed_data = knotHash(
+        std::ranges::to<std::vector<unsigned long>>(util::getNumbers(s)), 1
+    );
 
     return hashed_data[0] * hashed_data[1];
 }
 
 std::string
 part2(std::string const &s) {
-    std::vector<long> lengths;
-    std::vector<long> const tail {17, 31, 73, 47, 23};
+    std::vector<unsigned long> lengths;
+    std::vector<unsigned long> const tail {17, 31, 73, 47, 23};
     std::istringstream iss(s); // istringstream accepts string_view SINCE C++26
     char ch;
 
     while (iss.get(ch)) {
-        lengths.push_back(static_cast<long>(ch));
+        lengths.push_back(static_cast<unsigned long>(ch));
     }
     lengths.append_range(tail);
 
-    auto hashed_data = knotHash(lengths, 64);
+    auto hash = knotHash(lengths, 64);
     std::ostringstream oss;
-    for (auto i = 0; i < KNOTS_LEN; i += 16) {
+    for (auto it = hash.cbegin(); it < hash.cend(); std::advance(it, 16)) {
         auto h = std::ranges::fold_left(
-            hashed_data.begin() + i,
-            hashed_data.begin() + i + 16,
-            0,
-            std::bit_xor<long> {}
+            std::ranges::subrange(it, it + 16), 0, std::bit_xor<long> {}
         );
         oss << std::format("{:02x}", h & 0xFF);
     }
