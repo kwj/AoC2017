d6820ecf68f3ac662ca11b9ca1165532344e08b2
kwj <kwj@vlax.net>
2025-03-31 15:17:54 -0700

    Use std::mdspan if available (trial implementaion)

--- src/day19.cpp
+++ src/day19.cpp
@@ -6,10 +6,15 @@ module;
 #include <cstddef>
 #include <istream>
 #include <iterator>
+#if __cpp_lib_mdspan >= 202207L
+#include <mdspan>
+#endif
 #include <ranges>
 #include <sstream>
+#include <stdexcept>
 #include <string>
 #include <tuple>
+#include <unordered_set>
 #include <utility>
 #include <vector>
 
@@ -39,47 +44,107 @@ solve(std::istream &is) {
     return {part1(input_data), part2(input_data)};
 }
 
-/* What an ugly code */
+#if __cpp_lib_mdspan >= 202207L
 std::pair<std::string, long>
-followPath(std::vector<std::vector<char>> &grid) {
-    // find the start position and set the initial direction (down[dx=0,dy=1])
-    auto x = static_cast<size_t>(
-        std::ranges::distance(grid[0].begin(), std::ranges::find(grid[0], '|'))
+followPath(std::vector<char> const &grid_data, long rows, long cols) {
+    std::mdspan<const char, std::dextents<size_t, 2>> grid(
+        grid_data.data(), rows, cols
     );
-    auto y {0uz};
-    int dx {0}, dy {1};
+    auto r {0uz};
+    auto c = static_cast<size_t>(std::ranges::distance(
+        grid_data.cbegin(), std::ranges::find(grid_data, '|')
+    ));
+    int dr {1}, dc {0};
 
     std::ostringstream oss;
     long cnt {0};
-    while (grid[y][x] != ' ') {
-        if (grid[y][x] == '+') {
-            std::ranges::swap(dx, dy);
-            auto rev_dx {-dx}, rev_dy {-dy};
-            if (grid[y + static_cast<size_t>(rev_dy)]
-                    [x + static_cast<size_t>(rev_dx)] != ' ') {
-                dx = rev_dx;
-                dy = rev_dy;
+    while (grid[r, c] != ' ') {
+        if (grid[r, c] == '+') {
+            std::ranges::swap(dr, dc);
+            if (grid
+                    [r + static_cast<size_t>(-dr),
+                     c + static_cast<size_t>(-dc)] != ' ') {
+                dr = -dr;
+                dc = -dc;
             }
-        } else if (std::isalpha(grid[y][x])) {
-            oss << grid[y][x];
+        } else if (std::isalpha(grid[r, c])) {
+            oss << grid[r, c];
         }
 
-        x += static_cast<size_t>(dx);
-        y += static_cast<size_t>(dy);
+        r += static_cast<size_t>(dr);
+        c += static_cast<size_t>(dc);
         ++cnt;
     }
 
     return {oss.str(), cnt};
 }
+#else
+std::pair<std::string, long>
+followPath(std::vector<char> const &grid_data, long cols) {
+    std::vector<std::vector<char>> grid;
+    for (auto it = grid_data.cbegin(); it != grid_data.cend();
+         std::advance(it, cols)) {
+        grid.push_back(
+            std::ranges::subrange(it, it + cols) |
+            std::ranges::to<std::vector<char>>()
+        );
+    }
+
+    auto r {0uz};
+    auto c = static_cast<size_t>(std::ranges::distance(
+        grid_data.cbegin(), std::ranges::find(grid_data, '|')
+    ));
+    int dr {1}, dc {0};
+
+    std::ostringstream oss;
+    long cnt {0};
+    while (grid[r][c] != ' ') {
+        if (grid[r][c] == '+') {
+            std::ranges::swap(dc, dr);
+            if (grid[r + static_cast<size_t>(-dr)]
+                    [c + static_cast<size_t>(-dc)] != ' ') {
+                dr = -dr;
+                dc = -dc;
+            }
+        } else if (std::isalpha(grid[r][c])) {
+            oss << grid[r][c];
+        }
+
+        r += static_cast<size_t>(dr);
+        c += static_cast<size_t>(dc);
+        ++cnt;
+    }
+
+    return {oss.str(), cnt};
+}
+#endif
 
 std::pair<std::string, long>
 parse(std::istream &is) {
-    std::vector<std::vector<char>> grid;
+    std::vector<char> data;
+    std::unordered_set<long> cols;
+    [[maybe_unused]] long rows {0};
+
     for (std::string line; std::getline(is, line);) {
-        grid.push_back(line | std::ranges::to<std::vector<char>>());
+// P1206R7 (range to container conversion) is not yet fully supported in GCC 14.
+#if __cpp_lib_containers_ranges >= 202202L
+        data.append_range(line);
+#else
+        data.insert(data.end(), line.cbegin(), line.cend());
+#endif
+        cols.insert(std::ssize(line));
+        ++rows;
     }
 
-    return followPath(grid);
+    if (cols.size() != 1) {
+        throw std::runtime_error("Invalid format (number of columns)");
+    }
+
+#if __cpp_lib_mdspan >= 202207L
+    return followPath(data, rows, *cols.cbegin());
+#else
+    return followPath(data, *cols.cbegin());
+#endif
 }
 
 std::string
