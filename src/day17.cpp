42f017dac5f362b350b1207b519b867fcc9f2b42
kwj <kwj@vlax.net>
2025-03-31 11:41:23 -0700

    Tweak

--- src/day17.cpp
+++ src/day17.cpp
@@ -1,3 +1,68 @@
+/*
+[Part 2]
+from example (step = 3):
+
+- 0  5 (7) 2  4  3  6  1
+  buf_size = 8
+  pos = 2
+  n = 7
+
+- 0| 5 (7) |2  4  3| 6  1
+  -         --(1)-- --(2)-
+  repeat = (8 - 2 - 1) / 3 + 1 = 2
+ ==>
+  pos <- (2 + 2 * 3) % 8 + 1 = 0 + 1
+  buf_size <- 8 + 2 = 10
+  n <- 7 + 2 = 9
+
+results:
+- 0 (9) 5  7  2  4  3  #  6  1
+
+
+next:
+- 0| (9)|5  7  2| 4  3  #| 6  1
+  -    --(1)--  --(2)--  --(3)-
+  repeat = (10 - 1 - 1) / 3 + 1 = 3
+ ==>
+  pos <- (1 + 3 * 3) % 10 + 1 = 1
+  buf_size <- 10 + 3 = 13
+  n <- 9 + 3 = 12
+
+results:
+- 0 (12)  9  5  7  2  #  4  3  #  #  6  1
+
+and, so on.
+----------------
+
+With my given input data, it terminated after a 4,617 times loop.
+Naturally, it was dramatically faster than a naive fifty million times loop.
+
+  loop count      n
+  --------------------
+           ...
+     375         375
+     376         376
+     377         377
+     378         379
+     379         380
+           ...
+    1999       34386
+    2000       34482
+    2001       34578
+           ...
+    2999      555175
+    3000      556722
+    3001      558273
+           ...
+    3999     8963599
+    4000     8988567
+    4001     9013605
+           ...
+    4615    49732762
+    4616    49871293
+    4617    50010210
+*/
+
 module;
 
 #include <cstddef>
@@ -73,70 +138,5 @@ part2(long step) {
 
     return result;
 }
-
-/*
-[Part 2]
-from example (step = 3):
-
-- 0  5 (7) 2  4  3  6  1
-  buf_size = 8
-  pos = 2
-  n = 7
-
-- 0| 5 (7) |2  4  3| 6  1
-  -         --(1)-- --(2)-
-  repeat = (8 - 2 - 1) / 3 + 1 = 2
- ==>
-  pos <- (2 + 2 * 3) % 8 + 1 = 0 + 1
-  buf_size <- 8 + 2 = 10
-  n <- 7 + 2 = 9
-
-results:
-- 0 (9) 5  7  2  4  3  #  6  1
-
-
-next:
-- 0| (9)|5  7  2| 4  3  #| 6  1
-  -    --(1)--  --(2)--  --(3)-
-  repeat = (10 - 1 - 1) / 3 + 1 = 3
- ==>
-  pos <- (1 + 3 * 3) % 10 + 1 = 1
-  buf_size <- 10 + 3 = 13
-  n <- 9 + 3 = 12
-
-results:
-- 0 (12)  9  5  7  2  #  4  3  #  #  6  1
-
-and, so on.
-----------------
-
-With my given input data, it terminated after a 4,617 times loop.
-Naturally, it was dramatically faster than a naive fifty million times loop.
-
-  loop count      n
-  --------------------
-           ...
-     375         375
-     376         376
-     377         377
-     378         379
-     379         380
-           ...
-    1999       34386
-    2000       34482
-    2001       34578
-           ...
-    2999      555175
-    3000      556722
-    3001      558273
-           ...
-    3999     8963599
-    4000     8988567
-    4001     9013605
-           ...
-    4615    49732762
-    4616    49871293
-    4617    50010210
-*/
 
 } // namespace day17
